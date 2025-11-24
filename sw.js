b73a407fa12a344d82f13a50e00b76d2dfe6e8c1
rostangenius <rostangraf@gmail.com>
2025-11-25 00:12:17 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'repair-tracker-v1.0';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
